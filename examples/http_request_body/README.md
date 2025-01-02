8c39f9963c15847f27a31137f0e9a54cc3274acb
Premkumar Bhaskal <premkumar_bhaskal@intuit.com>
2025-01-02 18:12:44 +0530

    tested example to cache request body

---
+++ examples/http_request_body/README.md
@@ -0,0 +1,38 @@
+## Proxy-Wasm plugin example: HTTP headers
+
+Proxy-Wasm plugin that cached HTTP request body.
+
+### Building
+
+```sh
+% rustup toolchain install nightly
+% rustup target add wasm32-wasip1
+% cargo build --target wasm32-wasip1 --release
+% cp target/wasm32-wasip1/release/proxy_wasm_example_http_request_body.wasm /tmp/
+```
+
+### Using in Envoy
+
+This example can be run with [`docker compose`](https://docs.docker.com/compose/install/)
+and has a matching Envoy configuration.
+
+```sh
+% envoy -c envoy-local.yaml --log-level info
+```
+
+Send a POST request with a temporary file
+```sh
+% dd if=/dev/urandom of=/tmp/random.bin bs=500K count=1
+% cat /tmp/random.bin | base64  > /tmp/random.out            
+% curl -X POST http://localhost:10000/post -d@/tmp/random.out
+```
+
+
+Expected Envoy logs:
+```console
+[info][wasm] [...] wasm log http_headers: In method on_http_request_body, body_size: 393060, end_of_stream: false, content_length: 393060
+[info][wasm] [...] wasm log http_headers: In method on_http_request_body, body_size: 393060, end_of_stream: false, content_length: 786120
+[info][wasm] [...] wasm log http_headers: In method on_http_request_body, body_size: 682668, end_of_stream: true, content_length: 786120
+[info][wasm] [...] wasm log http_headers: Cached body length: 682668
+[info][wasm] [...] wasm log http_headers: #2 completed.
+```
---
+++ examples/http_request_body/src/lib.rs
@@ -0,0 +1,94 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use log::info;
+use proxy_wasm::traits::*;
+use proxy_wasm::types::*;
+
+proxy_wasm::main! {{
+    proxy_wasm::set_log_level(LogLevel::Trace);
+    proxy_wasm::set_root_context(|_| -> Box<dyn RootContext> { Box::new(HttpHeadersRoot) });
+}}
+
+struct HttpHeadersRoot;
+
+impl Context for HttpHeadersRoot {}
+
+impl RootContext for HttpHeadersRoot {
+    fn get_type(&self) -> Option<ContextType> {
+        Some(ContextType::HttpContext)
+    }
+
+    fn create_http_context(&self, context_id: u32) -> Option<Box<dyn HttpContext>> {
+        Some(Box::new(HttpHeaders { context_id, content_length: 0, cached_body: None }))
+    }
+}
+
+struct HttpHeaders {
+    context_id: u32,
+    content_length: usize,
+    cached_body: Option<Vec<u8>>,
+}
+
+impl Context for HttpHeaders {}
+
+impl HttpContext for HttpHeaders {
+    fn on_http_request_headers(&mut self, _: usize, _: bool) -> Action {
+        // for (name, value) in &self.get_http_request_headers() {
+        //     info!("#{} -> {}: {}", self.context_id, name, value);
+        // }
+
+        match self.get_http_request_header(":path") {
+            Some(path) if path == "/hello" => {
+                self.send_http_response(
+                    200,
+                    vec![("Hello", "World"), ("Powered-By", "proxy-wasm")],
+                    Some(b"Hello, World!\n"),
+                );
+                Action::Pause
+            }
+            _ => Action::Continue,
+        }
+    }
+
+    // When we pause before end_of_stream, then we don't have to accumulate body_size, envoy does this automatically.
+    fn on_http_request_body(&mut self, body_size: usize, end_of_stream: bool) -> Action {
+        if !end_of_stream {
+            self.content_length += body_size;
+            info!("In method on_http_request_body, body_size: {}, end_of_stream: {}, content_length: {}", body_size, end_of_stream, self.content_length);
+            return Action::Pause;
+        }
+        info!("In method on_http_request_body, body_size: {}, end_of_stream: {}, content_length: {}", body_size, end_of_stream, self.content_length);
+        self.cached_body = self.get_http_request_body(0, body_size);
+        // self.cached_body = self.get_http_request_body(0, self.content_length);
+
+        if let Some(ref body) = self.cached_body {
+            info!("Cached body length: {}", body.len());
+        } else {
+            info!("No body cached");
+        }
+        return Action::Continue;
+    }
+
+    fn on_http_response_headers(&mut self, _: usize, _: bool) -> Action {
+        // for (name, value) in &self.get_http_response_headers() {
+        //     info!("#{} <- {}: {}", self.context_id, name, value);
+        // }
+        Action::Continue
+    }
+
+    fn on_log(&mut self) {
+        info!("#{} completed.", self.context_id);
+    }
+}
